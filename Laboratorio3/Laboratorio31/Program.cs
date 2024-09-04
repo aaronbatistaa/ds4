3d4ccd524b7fb79251322a460e2006fcfbcd6983
Aaron Batista <arongomezbatista@outlook.com>
2024-09-03 19:50:50 -0500

    Implementación de Using y Reference para utilizar métodos del proyecto Laboratorio31 y solución completa del Laboratorio32

---
+++ Laboratorio3/Laboratorio31/Program.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualBasic;
+using System.Security.Cryptography.X509Certificates;
+using System = System.Math;
+
+namespace CalculoInicial
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Calculadora de diferencia de cuadrados");
+            CalculosMatematicos calculosMatematicos = new CalculosMatematicos();
+            Console.Write("Ingrese el número A: ");
+            calculosMatematicos.num1 = float.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese el número B: ");
+            calculosMatematicos.num2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("El resultado la diferencia de cuadrados de {0} y {1} es {2}", calculosMatematicos.num1, calculosMatematicos.num2, calculosMatematicos.CalculoDif());
+
+
+            Console.WriteLine("¡Muchas Gracias por su Cálculo!");
+        }
+    }
+
+    public class CalculosMatematicos
+    {
+        public float num1 { get; set; }
+        public float num2 { get; set; }
+        public double radio { get; set; }
+        public float CalculoDif()
+        {
+            return (num1 + num2) * (num1 - num2);
+        }
+
+        public double calculoArea()
+        {
+            return Math.Round(Math.PI * Math.Pow(radio, 2),2);
+        }
+    }
+}
---
+++ Laboratorio3/Laboratorio32/Program.cs
@@ -0,0 +1,16 @@
+﻿using CalculoInicial;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Calculadora de Área de un Círculo");
+        CalculosMatematicos calculosMatematicos = new CalculosMatematicos();
+        Console.Write("Ingrese el radio del Círculo: ");
+        calculosMatematicos.radio = double.Parse(Console.ReadLine());
+            
+        Console.WriteLine("El área del círculo cuyo radio es {0}, es de {1}", calculosMatematicos.radio, calculosMatematicos.calculoArea());
+
+
+        Console.WriteLine("¡Muchas Gracias por su Cálculo!");
+    }
+}
