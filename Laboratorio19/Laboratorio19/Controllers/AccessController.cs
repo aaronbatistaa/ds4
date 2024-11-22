05377ec4a8d0aa95e4dc9abe69427f4e236f81b0
Aaron Batista <arongomezbatista@outlook.com>
2024-11-22 15:16:12 -0500

    Laboratorio 19 Completado!

---
+++ Laboratorio19/Laboratorio19/Controllers/AccessController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Laboratorio19.Models.WS;
+using Microsoft.Ajax.Utilities;
+
+namespace Laboratorio19.Controllers
+{
+    public class AccessController : ApiController
+    {
+        [HttpGet]
+        public Reply HelloWorld()
+        {
+            Reply oR = new Reply();
+            oR.result = 1;
+            oR.message = "Mi Hello World en API";
+
+            return oR;
+        }
+    }
+}
---
+++ Laboratorio19/Laboratorio19/Models/WS/Reply.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace Laboratorio19.Models.WS
+{
+    public class Reply
+    {
+        public string message { get; set; }
+        public int result { get; set; }
+    }
+}
